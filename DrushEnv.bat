0dc75fd316b111d2db7dddb703ae67c4b719b5e1
john-carnegie <john.carnegie@invotra.co>
2016-09-08 15:32:58 +0100

    test

---
+++ DrushEnv.bat
@@ -0,0 +1,5 @@
+@ECHO Command Prompt shell optimized for Drush 
+@SET PATH=%PATH%;C:\ProgramData\Drush\
+@SET PATH=%PATH%;C:\wamp64\www\training\GnuWin32\bin
+@SET PATH=%PATH%;C:\wamp64\www\training\Php
+@SET PATH=%PATH%;C:\wamp64\www\training\cwRsync\bin
